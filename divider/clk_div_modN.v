7a647bc32ead89d87645631b981a18bf1ccc0dfb
huzefa-ee-vlsi <u24ee036@eed.svnit.ac.in>
2026-01-01 13:09:50 +0530

    Update clk_div_modN.v

--- divider/clk_div_modN.v
+++ divider/clk_div_modN.v
@@ -12,7 +12,7 @@ reg [W-1:0] neg_cnt;
 
 // --------POSITIVE EDGE--------//
 
-always@(posedge clk) begin
+    always@(posedge clk) begin
     if(rst)
      pst_cnt<=0;
     else if (pst_cnt==N-1)
@@ -23,15 +23,16 @@ end
 
 // --------NEGATIVE EDGE--------//
 
-always@(posedge clk) begin
+    always@(negedge clk) begin
     if(rst)
      neg_cnt<=0;
-    else if (pst_cnt==N-1)
+        else if (neg_cnt==N-1)
      neg_cnt<=0;
     else 
      neg_cnt<=neg_cnt+1;
 end
 
 assign clk_out=(pst_cnt==N-1)|(neg_cnt==N-1);
+
 
 endmodule //clk_div_modN
